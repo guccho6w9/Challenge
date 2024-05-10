253538c2ff9d69959315f9099fec5fb4d297a62b
guccho6w9 <gusta.quiroga033@gmail.com>
2024-05-10 18:06:49 -0300

    add(-): added hover to the movies

--- challenge-star-wars/src/pages/films/[id].js
+++ challenge-star-wars/src/pages/films/[id].js
@@ -1,6 +1,9 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
+import '@/app/globals.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Banner from '@/pages/banner';
 import Navbar from '@/pages/navbar';
 import Footer from '@/pages/footer';
@@ -9,6 +12,7 @@ const FilmDetailPage = () => {
     const router = useRouter();
     const [film, setFilm] = useState(null);
     const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true); // Estado para controlar la carga de datos
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -26,16 +30,32 @@ const FilmDetailPage = () => {
                     })
                 );
                 setCharacters(charactersData);
+                setLoading(false); // Marcar como completada la carga de datos
             } catch (error) {
                 setError(error);
+                setLoading(false); // Marcar como completada la carga de datos
             }
         };
 
         fetchFilm();
     }, [router.query.id]);
 
+    if (loading) {
+        return (
+            <div>
+                <Banner />
+                <Navbar />
+                <div className="h-0.5 bg-white w-full"></div>
+                <div className="flex justify-center mt-4">
+                    <FontAwesomeIcon icon={faSpinner} spin className="text-4xl text-white" /> {/* Rueda de cargando color blanco */}
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+
     if (!film) {
-        return <div>Cargando...</div>;
+        return <div>No se pudo cargar la película.</div>;
     }
 
     return (
@@ -49,21 +69,22 @@ const FilmDetailPage = () => {
                         <img className="h-auto w-full" src="/images/films-images/generic-image.png" alt="Imagen genérica" />
                     </div>
                     <div>
-                        <h1 className="text-3xl">{film.title}</h1>
-                        <p>Episodio: {film.episode_id}</p>
+                        <h1 className="text-5xl mb-6">{film.title}</h1>
+                        <h2 className='text-3x1'>Episodio: {film.episode_id}</h2>
                         <p>Director: {film.director}</p>
                         <p>Opening crawl: {film.opening_crawl}</p>
                     </div>
                 </div>
                 <div>
-                    <h2 className="text-3xl">Personajes:</h2>
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                    <div className="h-0.5 bg-white w-full mb-6"></div>
+                    <h2 className="text-3xl font-bold mb-5"> Personajes </h2>
+                    <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 ">
                         {characters.map((character) => (
                             <div key={character.url}>
                                 <Link href={`/character/${character.url.split('/').slice(-2)[0]}`} legacyBehavior>
                                     <a>
-                                        <img className="w-full h-auto" src="/images/characters-images/generic-image.png" alt="Imagen genérica" />
-                                        <p>{character.name}</p>
+                                        <img className="w-full h-auto mb-2" src="/images/characters-images/generic-image.png" alt="Imagen genérica" />
+                                        <p className='font-bold'>{character.name}</p>
                                     </a>
                                 </Link>
                             </div>
--- challenge-star-wars/src/pages/films/films.js
+++ challenge-star-wars/src/pages/films/films.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import '@/app/globals.css';
+
 import Banner from '@/pages/banner';
 import Navbar from '@/pages/navbar';
 import Footer from '@/pages/footer';
 
 const FilmsPage = () => {
   const [films, setFilms] = useState([]);
+  const [loading, setLoading] = useState(true); // Estado para controlar la carga de datos
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -18,8 +22,10 @@ const FilmsPage = () => {
         }
         const data = await response.json();
         setFilms(data.results);
+        setLoading(false); // Marcar como completada la carga de datos
       } catch (error) {
         setError(error);
+        setLoading(false); // Marcar como completada la carga de datos
       }
     };
 
@@ -30,25 +36,37 @@ const FilmsPage = () => {
     <div>
       <Banner />
       <Navbar />
-      <div className="h-0.5 bg-white w-full"></div>
-      <h1 className="text-center"> Películas </h1>
-      <div className="grid grid-cols-5 gap-4 justify-center">
-        {films.map((film) => (
-          <div key={film.url}>
-            <Link href={`/films/${film.url.split('/').slice(-2)[0]}`} legacyBehavior>
-              <a className="flex flex-col items-center">
-                <img className="w-58 h-80" src="/images/films-images/generic-image.png" alt="Imagen genérica" />
-                <h2 className="text-center">{film.title}</h2>
-                <p className="text-center">Episodio: {film.episode_id}</p>
-              </a>
-            </Link>
-          </div>
-        ))}
-      </div>
-     
+      <div className="h-0.5 bg-white w-full mb-6"></div>
+      <h1 className="text-center text-4xl font-bold mb-12"> PELÍCULAS </h1>
+
+      {/* Mostrar la rueda de carga si loading es verdadero */}
+      {loading && (
+        <div className="flex justify-center mt-4">
+          <FontAwesomeIcon icon={faSpinner} spin className="text-4xl text-white" /> {/* Rueda de cargando color blanco */}
+        </div>
+      )}
+
+      {/* Mostrar las películas si loading es falso */}
+      {!loading && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-4 justify-center">
+          {films.map((film) => (
+            <div key={film.url} className="hover:bg-white hover:bg-opacity-15  rounded-lg transition duration-300">
+              <Link href={`/films/${film.url.split('/').slice(-2)[0]}`} legacyBehavior>
+                <a className="flex flex-col items-center">
+                  <img className="w-58 h-80" src="/images/films-images/generic-image.png" alt="Imagen genérica" />
+                  <h2 className="text-center font-bold">{film.title}</h2>
+                  <p className="text-center">Episodio: {film.episode_id}</p>
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+
       <Footer />
       {error && <p>Error al cargar las películas: {error.message}</p>}
     </div>
   );
 };
+
 export default FilmsPage;
