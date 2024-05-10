e227a088219b73129d6cc892f90f1bfe711ad669
guccho6w9 <gusta.quiroga033@gmail.com>
2024-05-09 21:05:33 -0300

    Add: updated index.js file with a carousel, deleted header

--- challenge-star-wars/src/pages/index.js
+++ challenge-star-wars/src/pages/index.js
@@ -1,54 +1,57 @@
-
-import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; {/* decidi usar axios para traer los datos de la api*/}
+import '@/app/globals.css';
+//bibliotecas de font awesome para algunos iconos
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import Banner from '@/pages/banner';
+import Navbar from '@/pages/navbar';
+import Footer from '@/pages/footer';
 
 function HomePage() {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    
+    //imagenes para el carrusel
+    const [currentImage, setCurrentImage] = useState(0);
+    const images = [
+        "/images/carrusel/esperanza.jpeg",
+        "/images/carrusel/imperio.jpeg",
+        "/images/carrusel/retorno.jpeg"
+    ];
+
+    // Función para avanzar al siguiente imagen
+    const nextImage = () => {
+        setCurrentImage((currentImage + 1) % images.length);
+    };
+
+    // Función para retroceder a la imagen anterior
+    const prevImage = () => {
+        setCurrentImage((currentImage - 1 + images.length) % images.length);
+    };
+
+    // Función para cambiar automáticamente la imagen cada 5 segundos
     useEffect(() => {
-        // Realizar la solicitud a la API de SWAPI
-        axios.get('https://swapi.dev/api/')
-            .then(response => {
-                setData(response.data); // Almacenar los datos en el estado
-            })
-            .catch(error => {
-                setError(error); // Manejar errores si la solicitud falla
-            });
-    }, []); // Ejecutar solo una vez al montar el componente
+        const interval = setInterval(nextImage, 5000);
+        return () => clearInterval(interval);
+    }, [currentImage]);
+
+
 
     return (
-        <div className="container mx-auto">
-            <h1 className="text-4xl font-bold text-center mt-8">¡Bienvenido a la Galaxia de Star Wars!</h1>
-
-
-            {/*Div para los botones de las paginas peliculas y personajes*/}
-            <div className="flex justify-center mt-4">
-                <Link href="/films" legacyBehavior>
-                    <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-                        Ver Películas
-                    </a>
-                </Link>
-                <Link href="/characters" legacyBehavior>
-                    <a className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                        Ver Personajes
-                    </a>
-                </Link>
+        <div>
+            <Banner /> 
+            <Navbar /> 
+            <div className="h-0.5 bg-white w-full"></div>
+            <div className="text-center mt-8">
+                <h1 className="text-4xl font-bold">¡Bienvenido a la Galaxia de Star Wars!</h1>
+                <div className="carousel relative">
+                    <button onClick={prevImage} className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black text-white px-4 py-2 rounded">
+                        <FontAwesomeIcon icon={faChevronLeft} />
+                    </button>
+                    <button onClick={nextImage} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black text-white px-4 py-2 rounded">
+                        <FontAwesomeIcon icon={faChevronRight} />
+                    </button>
+                    <img src={images[currentImage]} alt={`Slide ${currentImage + 1}`} className="w-full h-auto" />
+                </div>
             </div>
-
-
-
-            <div className="flex justify-center mt-4">
-                {/* Mostrar los datos obtenidos de SWAPI si están disponibles para testear datos */}
-                {data && (
-                    <pre>{JSON.stringify(data, null, 2)}</pre>
-                )}
-                {/* Manejar errores si la solicitud falla */}
-                {error && (
-                    <p>Error al obtener datos de SWAPI: {error.message}</p>
-                )}
-            </div>
+            <Footer />
         </div>
     );
 }
