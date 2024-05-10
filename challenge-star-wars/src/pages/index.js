74589689fa09656333efe824eb004023d934e9c3
guccho6w9 <gusta.quiroga033@gmail.com>
2024-05-10 18:06:02 -0300

    add(-): added link tag to the carousel for their respective  movies

--- challenge-star-wars/src/pages/index.js
+++ challenge-star-wars/src/pages/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import '@/app/globals.css';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import '@/app/globals.css';
 import Banner from '@/pages/banner';
 import Navbar from '@/pages/navbar';
 import Footer from '@/pages/footer';
@@ -12,18 +13,22 @@ function HomePage() {
     const images = [
         {
             src: "/images/carrusel/esperanza.jpeg",
-            title: "Una nueva esperanza"
+            title: "Una nueva esperanza",
+            filmId: 1
         },
         {
             src: "/images/carrusel/imperio.jpeg",
-            title: "El Imperio contraataca"
+            title: "El Imperio contraataca",
+            filmId: 2
         },
         {
             src: "/images/carrusel/retorno.jpeg",
-            title: "El retorno del Jedi"
+            title: "El retorno del Jedi",
+            filmId: 3
         }
     ];
-    //funcion para moverse entre imagenes mediante los puntos
+
+    // Función para moverse entre imágenes mediante los puntos
     const handleImageClick = (index) => {
         setCurrentImage(index);
     };
@@ -43,40 +48,43 @@ function HomePage() {
 
     return (
         <div>
-            <Banner /> 
-            <Navbar /> 
-            <div className="h-0.5 bg-white w-full"></div>
+            <Banner />
+            <Navbar />
+            <div className="h-0.5 bg-white w-full mb-3"></div>
             <div className="text-center mt-8">
-                <h1 className="text-4xl font-bold">Peliculas destacadas </h1>
+                <h1 className="text-4xl font-bold">PELÍCULAS DESTACADAS </h1>
                 <div className="carousel relative w-200">
-                    
+
                     <div className="relative">
-                        <img src={images[currentImage].src} alt={`Slide ${currentImage + 1}`} className="w-full h-auto rounded-lg" />
-                        {/* caja negra con el titulo de la pelicula en carrusel */}
+                        <Link legacyBehavior href={`/films/${images[currentImage].filmId}` }>
+                            <a>
+                                <img src={images[currentImage].src} alt={`Slide ${currentImage + 1}`} className="w-full h-auto rounded-lg" />
+                            </a>
+                        </Link>
+                        {/* Caja negra con el título de la película en carrusel */}
                         <div className="absolute top-0 right-0 bg-black bg-opacity-50 text-white p-2 h-full w-1/2 flex items-center justify-center">
                             <h2 className="sm:text-3xl lg:text-6xl xl:text-7xl text-lg font-bold">{images[currentImage].title}</h2>
                         </div>
                     </div>
 
-                    {/* botones para desplazarse entre entre las imagenes  */}
-                    {/* boton izqquierda  */}
-                    <button onClick={prevImage} className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black text-white px-4 py-2 rounded-full">
+                    {/* Botones para desplazarse entre las imágenes */}
+                    {/* Botón izquierda */}
+                    <button onClick={prevImage} className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black text-white px-4 py-2 rounded-full bg-opacity-50">
                         <FontAwesomeIcon icon={faChevronLeft} />
                     </button>
-                    {/* boton derecha  */}
-                    <button onClick={nextImage} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black text-white px-4 py-2 rounded-full">
+                    {/* Botón derecha */}
+                    <button onClick={nextImage} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black text-white px-4 py-2 rounded-full bg-opacity-50">
                         <FontAwesomeIcon icon={faChevronRight} />
                     </button>
 
-
-                    {/* puntos circulares para desplazarse entre imagenes  */}
+                    {/* Puntos circulares para desplazarse entre imágenes */}
                     <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-                        {/* crear puntos para cada imagen */}
+                        {/* Crear puntos para cada imagen */}
                         {images.map((_, index) => (
                             <FontAwesomeIcon
                                 key={index}
                                 icon={faCircle}
-                                className={`text-2xl sm:text-1xl lg:text-2xl xl:text-3xl cursor-pointer ${currentImage === index ? 'text-white' : 'text-gray-400'}`}
+                                className={`text-lg sm:text-xl lg:text-2xl xl:text-3xl cursor-pointer ${currentImage === index ? 'text-white' : 'text-gray-400'} mt-20`}
                                 onClick={() => handleImageClick(index)}
                             />
                         ))}
