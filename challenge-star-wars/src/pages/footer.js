583701a0b7176deaacbb471d5cea767a9a556650
guccho6w9 <gusta.quiroga033@gmail.com>
2024-05-09 21:07:07 -0300

    Add: added footer.js

---
+++ challenge-star-wars/src/pages/footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white text-center py-4">
+            <p>TM &amp; © Lucasfilm Ltd. All Rights Reserved</p>
+            <p>
+                Hecho por Gustavo A. Quiroga |{' '}
+                <a href="mailto:gusta.quiroga033@gmail.com" className="underline">gusta.quiroga033@gmail.com</a>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
