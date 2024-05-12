3b10bfc88a8d395d56a47d65d05bb76be2ee638d
guccho6w9 <gusta.quiroga033@gmail.com>
2024-05-12 17:08:44 -0300

    Refactor(-):Updated the filter to include gender = none

--- challenge-star-wars/src/pages/character/[id].js
+++ challenge-star-wars/src/pages/character/[id].js
@@ -1,10 +1,9 @@
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import '@/app/globals.css';
-import Banner from '@/pages/banner';
-import Navbar from '@/pages/navbar';
-import Footer from '@/pages/footer';
+import Header from '@/components/header'; //importado banner.js
+import Navbar from '@/components/navbar'; //importado navbar.js
+import Footer from '@/components/footer'; //importado footer.js
 
 const CharacterDetailPage = () => {
     const router = useRouter();
@@ -32,27 +31,42 @@ const CharacterDetailPage = () => {
     }, [id]);
 
     return (
-        <div>
-            <Banner />
+        <div className="flex flex-col items-center">
+            <Header />
             <Navbar />
-            <div className="h-0.5 bg-white w-full"></div>
+            <div className="h-0.5 bg-white w-full" ></div>
             {character ? (
-                <div>
-                    <h1>{character.name}</h1>
-                    <img src="/images/characters-images/generic-image.png" alt="Imagen genérica de personaje" className="h-76 w-64" />
-                    {character.eye_color !== 'n/a' && character.eye_color !== 'unknown' && (
-                        <p>Color de ojos: {character.eye_color}</p>
+                <div className="text-center mb-40">
+                    <h1 className="text-3xl font-bold mb-6 mt-4">{character.name}</h1>
+                    <img src="/images/characters-images/generic-image.png" alt="Imagen genérica de personaje" className="h-76 w-64 mb-4" />
+                    {character.birth_year !== 'n/a' && character.birth_year !== 'unknown' && (
+                        <p>Año de nacimiento: {character.birth_year}</p>
                     )}
-                    <p>Año de nacimiento: {character.birth_year}</p>
                     {character.hair_color !== 'n/a' && character.hair_color !== 'unknown' && (
                         <p>Color de pelo: {character.hair_color}</p>
                     )}
-                    <p>Altura: {character.height} cm</p>
-                    <p>Color de piel: {character.skin_color}</p>
-                    <p>Masa: {character.mass} </p>
+                    {character.eye_color !== 'n/a' && character.eye_color !== 'unknown' && (
+                        <p>Color de ojos: {character.eye_color}</p>
+                    )}
+                    {character.skin_color !== 'n/a' && character.skin_color !== 'unknown' &&(
+                        <p>Color de piel: {character.skin_color} </p>
+
+                    )}
+                    {character.height !== 'n/a' && character.height !== 'unknown' && (
+                        <p>Altura: {character.height} cm</p>
+
+                    )}
+
+
+                    {character.mass !== 'n/a' && character.mass !== 'unknown' && (
+                        <p>Masa: {character.mass} </p>
+
+                    )}
+
+
                 </div>
             ) : (
-                <p>Cargando...</p>
+                <p className="mt-4">Cargando...</p>
             )}
             {error && <p>Error: {error.message}</p>}
             <Footer />
--- challenge-star-wars/src/pages/character/characters.js
+++ challenge-star-wars/src/pages/character/characters.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import Banner from '@/pages/banner';
-import Navbar from '@/pages/navbar';
-import Footer from '@/pages/footer';
+import { faFilter, faSpinner, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+import Header from '@/components/header'; //importado banner.js
+import Navbar from '@/components/navbar'; //importado navbar.js
+import Footer from '@/components/footer'; //importado footer.js
 import '@/app/globals.css';
+
+
+
 
 const CharactersPage = () => {
     const [characters, setCharacters] = useState([]);
@@ -19,7 +22,11 @@ const CharactersPage = () => {
         gender: 'all'
     });
     const [showFilterMenu, setShowFilterMenu] = useState(false);
+    const [selectedCharacterIndex, setSelectedCharacterIndex] = useState(null);
 
+
+
+    // Hook de efecto para cargar los personajes desde la API, decidi usar un bucle con promise para traer todas las paginas de personajes de una vez por la limitacion de swapi de tener solo 10 personajes por pagina, tarde solo un poco mas al cargar la primera vez, luego de eso la navegacion es mas fluida
     useEffect(() => {
         const fetchCharacters = async () => {
             try {
@@ -41,6 +48,7 @@ const CharactersPage = () => {
         fetchCharacters();
     }, []);
 
+    // Hook de efecto para filtrar los personajes cuando cambian los filtros
     useEffect(() => {
         let filtered = characters.filter(character => {
             if (filterOptions.eyeColor !== 'all') {
@@ -49,8 +57,14 @@ const CharactersPage = () => {
                     return false;
                 }
             }
-            if (filterOptions.gender !== 'all' && character.gender !== filterOptions.gender) {
-                return false;
+            //no estaba especificado en el pdf el genero "none" asi que lo filtro junto a n/a aqui en la funcion, se muestra en el sistema
+            if (filterOptions.gender !== 'all') {
+                if (filterOptions.gender === 'n/a') {
+                    // si se selecciona "No especificado", filtrara tanto "n/a" como "none"
+                    return character.gender === 'n/a' || character.gender === 'none';
+                } else {
+                    return character.gender === filterOptions.gender;
+                }
             }
             return true;
         });
@@ -61,23 +75,26 @@ const CharactersPage = () => {
         setCurrentPage(1);
     }, [characters, filterOptions]);
 
+    //obteniene los personajes para la página actual
     const paginatedCharacters = filteredCharacters.slice((currentPage - 1) * 10, currentPage * 10);
 
+    //funcion para moverse a siguiente pagina
     const handleNextPage = () => {
         setCurrentPage(prevPage => prevPage + 1);
     };
-
+    //funcion para ir a la pagina anterior
     const handlePrevPage = () => {
         setCurrentPage(prevPage => prevPage - 1);
     };
 
+    //maneja las opciones de filtro al cambiarlo
     const handleFilterOptionChange = (type, value) => {
         setFilterOptions(prevOptions => ({
             ...prevOptions,
             [type]: value
         }));
     };
-
+    //retablece los filtros
     const handleResetFilters = () => {
         setFilterOptions({
             eyeColor: 'all',
@@ -85,9 +102,23 @@ const CharactersPage = () => {
         });
     };
 
+
+    //funciones de hover
+    const handleCharacterMouseEnter = (index) => {
+        setSelectedCharacterIndex(index);
+    };
+
+    const handleCharacterMouseLeave = () => {
+        setSelectedCharacterIndex(null);
+    };
+
+
     return (
+
+
         <div style={{ position: 'relative' }}>
-            <Banner />
+
+            <Header />
             <Navbar />
             <div className="h-0.5 bg-white w-full mb-6"></div>
             <h1 className="text-4xl text-center font-bold mb-6"> PERSONAJES </h1>
@@ -96,7 +127,7 @@ const CharactersPage = () => {
                 <div className="relative">
                     <FontAwesomeIcon
                         icon={faFilter}
-                        className="text-gray-500 cursor-pointer"
+                        className="text-gray-500 cursor-pointer" size="2x"
                         onClick={() => setShowFilterMenu(!showFilterMenu)}
                     />
                     {showFilterMenu && (
@@ -151,11 +182,26 @@ const CharactersPage = () => {
                 </div>
             )}
 
-            {!loading && (
-                <div className="flex flex-wrap justify-center mt-4">
-                    {paginatedCharacters.map(character => (
+            {!loading && filteredCharacters.length === 0 ? ( // Check if there are no matches
+                <div className="flex flex-col items-center mt-4">
+                    <img src="/images/characters-images/no-match.png" alt="No hay coincidencias" className="h-60 w-60 mb-4" />
+                    <p className='mb-3 text-center'>"¡Meesa buscando, pero no meesa encontrar nada! ¡Parece que estamos en el lugar equivocado!"</p>
+                    <p className='font-bold text-lg'>No se encontraron resultados </p>
+
+        
+                </div>
+            ) : (
+                <div className="flex flex-wrap justify-center mt-4 mb-10">
+                    {paginatedCharacters.map((character, index) => (
                         <Link key={character.url} href={`/character/${character.url.split('/').slice(-2)[0]}`} legacyBehavior>
-                            <a className="flex flex-col items-center justify-center space-y-2 m-2 w-1/3 sm:w-1/4 md:w-1/6 lg:w-1/6 xl:w-1/6 p-2 hover:bg-white hover:bg-opacity-15 rounded shadow-md transition duration-300 ease-in-out transform hover:scale-105 relative">
+
+                            {/* caja de personajes, mostrara 5 personajes por fila en pantallas grandes, 3 en tables y 2 en celulares. tiene efecto hover */}
+                            <a
+                                className="flex flex-col items-center justify-center space-y-2 m-2 w-1/3 sm:w-1/5 md:w-1/4 lg:w-1/6 xl:w-1/5 p-2 hover:bg-white hover:bg-opacity-15 rounded shadow-md transition duration-300 ease-in-out transform hover:scale-105 relative"
+                                onMouseEnter={() => handleCharacterMouseEnter(index)}
+                                onMouseLeave={handleCharacterMouseLeave}
+                            >
+                                {/* informacion de los personajes traida de la api */}
                                 <div className="h-80 flex flex-col">
                                     <img src="/images/characters-images/generic-image.png" alt="Imagen genérica de personaje" className="w-38 h-auto mb-3" />
                                     <div className="text-center">
@@ -166,6 +212,16 @@ const CharactersPage = () => {
                                         {character.gender !== 'n/a' && character.gender !== 'unknown' && (
                                             <p>Género: {character.gender}</p>
                                         )}
+
+                                        {/* icono que agregue que aparece al hacerle hover al personaje, es solo estetico */}
+                                        {selectedCharacterIndex === index && (
+                                            <div className=" bottom-0">
+                                                <FontAwesomeIcon
+                                                    icon={faChevronDown}
+                                                    className="text-white text-2xl"
+                                                />
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
                             </a>
@@ -174,6 +230,7 @@ const CharactersPage = () => {
                 </div>
             )}
 
+            {/* botones para moverse entre el paginado */}
             <div className="flex justify-center mt-4">
                 {currentPage > 1 && (
                     <button onClick={handlePrevPage} className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-4 border border-white">Anterior</button>
